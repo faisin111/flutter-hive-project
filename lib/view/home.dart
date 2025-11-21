f74c92e61f4c5cec399a559dbbf3f35b65074450
faisin111 <faisins16@gmail.com>
2025-11-21 16:20:13 +0530

    message

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   List<StudentRecord> _student = [];
 
   File? _selectedImage;
+  
   final ImagePicker _picker = ImagePicker();
 
   Future<void> loadStudent() async {
